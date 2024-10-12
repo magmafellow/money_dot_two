01c2f02dc26d5638f68e29ab9a4672afdc6c58be
magmafellow <magmafellow@gmail.com>
2024-10-12 17:59:14 +0300

    first block finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Inter } from 'next/font/google'
 import "./globals.css";
 
 const inter = Inter({weight: ['300', '400', '500', '600', '700', '800', '900'], subsets: ['cyrillic', 'latin']})
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Landing page | Money 2.0",
